e86a9c74cce376e0425bee5072de6c71a81dce3b
Alex0215 <kr.naman007@gmail.com>
2023-12-07 01:13:56 +0530

    full site revamp

--- E Commerce/client/src/components/layout/footer.js
+++ E Commerce/client/src/components/layout/footer.js
@@ -8,128 +8,6 @@ import ScrollButton from "../forms/ScrollButton";
 import {} from "antd";
 import "../../styles/footer.css";
 
-//     <div>
-//       {/* <div className="container-footer"> */}
-//       <div className="col-md-14 col-lg-13">
-//         <div className="container-footer">
-//           <NavLink to="/" style={{ textDecoration: "none" }}>
-//             <span
-//               className="footer-logo"
-//               style={{
-//                 color: "Red",
-//                 fontSize: "29px",
-//                 fontFamily: "lost-treasure",
-//                 cursor: "pointer",
-//                 marginLeft: "40px",
-//               }}
-//             >
-//               Rohini Helmets
-//             </span>
-//           </NavLink>
-//           <p className="footer-info-text">
-//             Reference site about Lorem Ipsum, giving information on its origins,
-//             as well as a random Lipsum generator.
-//           </p>
-//           <div className="col-md-12">
-//             <div className="footer-menu">
-//               <ul>
-//                 <li>
-//                   <NavLink to="/" className="nav-link" aria-current="page">
-//                     Home
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="#" className="nav-link" aria-current="page">
-//                     Terms
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink
-//                     to="/policy"
-//                     className="nav-link"
-//                     aria-current="page"
-//                   >
-//                     Privacy Policy
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink
-//                     to="/contact"
-//                     className="nav-link"
-//                     aria-current="page"
-//                   >
-//                     Contact
-//                   </NavLink>
-//                 </li>
-//               </ul>
-//             </div>
-//             <div className="footer-social-link">
-//               <h3>Follow us</h3>
-//               <ul className="ul-1">
-//                 <li>
-//                   <Link
-//                     className="li-1 footer-social-link facebook"
-//                     to="https://www.facebook.com/HelmetMusic/"
-//                   >
-//                     <FaFacebook /> : Facebook
-//                   </Link>
-//                 </li>
-//                 <li>
-//                   <Link
-//                     className="li-1 footer-social-link instagram"
-//                     to="https://www.instagram.com/helmets_for_india/?hl=en"
-//                   >
-//                     <FaInstagram /> : Instagram
-//                   </Link>
-//                   <li>
-//                     <Link
-//                       className="li-1 footer-social-link whatsapp"
-//                       to="Whatsapp"
-//                     >
-//                       <FaWhatsapp /> : Whatsapp
-//                     </Link>
-//                   </li>
-//                   <li className="li-1">
-//                     <BiMailSend /> : dummy@email.com
-//                   </li>
-//                   <li className="li-1">
-//                     <BiPhoneCall /> : 012-3456789
-//                   </li>
-//                   <li className="li-1">
-//                     <BiSupport /> : 1800-0000-0000 (toll free)
-//                   </li>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//           {/* </div> */}
-//           {/* </div> */}
-//         </div>
-
-//         <div className="copyright">
-//           <div className="container-end-footer">
-//             <div className="row">
-//               <div className="col-md-12">
-//                 <span>Copyright © 2019, All Right Reserved Seobin</span>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div id="back-to-top" className="back-to-top">
-//         <button
-//           className="btn btn-dark"
-//           title="Back to Top"
-//           style={{ display: "block" }}
-//         >
-//           <BiSolidUpArrow />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
 const Footer = () => {
   const scrollToTop = () => {
     window.scrollTo(0, 0); // Scroll to the top of the page
@@ -148,14 +26,13 @@ const Footer = () => {
                 cursor: "pointer",
               }}
             >
-              Rohini Helmets
+              Artysly
             </span>
           </NavLink>
         </div>
         <div id="item-1">
           <p className="footer-info-text">
-            "Ride in style, ride in safety – our helmet collection offers the
-            best of both worlds for discerning riders."
+          "Canvas Your World, Paint Your Soul"
           </p>
         </div>
         <div id="item-2">
@@ -189,13 +66,10 @@ const Footer = () => {
             </ul>
             <ul className="ul-2">
               <li className="li-2">
-                <BiMailSend /> : rohini_helmet@gmail.com
+                <BiMailSend /> : Artysly_paint@gmail.com
               </li>
               <li className="li-2">
-                <BiPhoneCall id="phone" /> : 098914 08448
-              </li>
-              <li className="li-2">
-                <BiPhoneCall id="phone" /> : 098100 57030
+                <BiPhoneCall id="phone" /> : 097714 99948
               </li>
             </ul>
           </div>
@@ -245,44 +119,15 @@ const Footer = () => {
                 </NavLink>
               </li>
             </ul>
-            <h2 id="heading-3">OPENING HOURS</h2>
-            <ul className="ul-4">
-              <li className="li-4">Mon: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Tue: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Wed: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Thu: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Fri: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Sat: 9:00 am - 10:00 pm</li>
-              <li className="li-4">Sun: 9:00 am - 10:30 pm</li>
-            </ul>
-            {/* <form>
-              <input
-                type="email"
-                id="emailInput"
-                placeholder="Enter your email"
-              />
-              <button type="submit">Subscribe</button>
-            </form> */}
-            {/* FORM KA INPUT TYPE */}
           </div>
         </div>
         <div id="item-4">
-          <iframe
-            title="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3498.676014414946!2d77.1250143752211!3d28.729226875612085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d016a4902905d%3A0xe26f6b10a8901011!2sROHINI%20Helmets!5e0!3m2!1sen!2sin!4v1692607916253!5m2!1sen!2sin"
-            width="100%"
-            height="100%"
-            // style="border:0;"
-            // className="item-4-map"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+        {/* <img className="footer-img" src="https://i.pinimg.com/564x/d9/67/94/d96794db6809b79e6b78abb34789f0a3.jpg"  frameborder="0" scrolling="no" alt="Paris"/> */}
         </div>
       </div>
       <div id="item-5">
         <div className="copyright">
-          <span>Copyright © 2022, ROHINI HELMETS All Rights reserved</span>
+          <span>Copyright © 2022, Artysly All Rights reserved</span>
         </div>
       </div>
       <ScrollButton />
--- E Commerce/client/src/components/prices.js
+++ E Commerce/client/src/components/prices.js
@@ -1,42 +1,42 @@
 export const prices = [
   {
     _id: 0,
-    name: "₹0 to 999",
+    name: "₹0 to 199",
     array: [0, 999],
   },
   {
     _id: 1,
-    name: "₹1000 to 1999",
-    array: [1000, 1999],
+    name: "₹200 to 399",
+    array: [200, 399],
   },
   {
     _id: 2,
-    name: "₹2000 to 2999",
-    array: [2000, 2999],
+    name: "₹400 to 599",
+    array: [400, 599],
   },
   {
     _id: 3,
-    name: "₹3000 to 3999",
-    array: [3000, 3999],
+    name: "₹600 to 799",
+    array: [600, 799],
   },
   {
     _id: 4,
-    name: "₹4000 to 4999",
-    array: [4000, 4999],
+    name: "₹800 to 999",
+    array: [800, 999],
   },
   {
     _id: 5,
-    name: "₹5000 to 5999",
-    array: [5000, 5999],
+    name: "₹1000 to 1199",
+    array: [1000, 1199],
   },
   {
     _id: 6,
-    name: "₹6000 to 6999",
-    array: [6000, 6999],
+    name: "₹1200 to 1399",
+    array: [1200, 1399],
   },
   {
     _id: 7,
-    name: "₹7000 or more",
-    array: [7000, 99999],
+    name: "₹1399 or more",
+    array: [1399, 99999],
   },
 ];
